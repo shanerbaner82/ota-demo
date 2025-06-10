777bdee4b3f058e43d0449112182a62956aafad9
Shane Rosenthal <srosenthal82@gmail.com>
2025-06-10 17:02:36 +0100

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,6 +17,6 @@
        @vite('resources/css/app.css')
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] dark:text-white flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <p class="text-2xl">Hello World</p>
+        <p class="text-2xl">Hello Laravel Live UK</p>
     </body>
 </html>
