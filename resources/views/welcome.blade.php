e035b1a7ee2fd41efdf0214f20f3f943f53b88bb
Shane Rosenthal <srosenthal82@gmail.com>
2025-06-17 11:41:22 -0400

    adds badges

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,6 +20,24 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] dark:text-white flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <p class="text-2xl">Hello Dude!</p>
         <flux:profile avatar="https://unavatar.io/x/calebporzio" />
+        <flux:badge color="zinc">Zinc</flux:badge>
+        <flux:badge color="red">Red</flux:badge>
+        <flux:badge color="orange">Orange</flux:badge>
+        <flux:badge color="amber">Amber</flux:badge>
+        <flux:badge color="yellow">Yellow</flux:badge>
+        <flux:badge color="lime">Lime</flux:badge>
+        <flux:badge color="green">Green</flux:badge>
+        <flux:badge color="emerald">Emerald</flux:badge>
+        <flux:badge color="teal">Teal</flux:badge>
+        <flux:badge color="cyan">Cyan</flux:badge>
+        <flux:badge color="sky">Sky</flux:badge>
+        <flux:badge color="blue">Blue</flux:badge>
+        <flux:badge color="indigo">Indigo</flux:badge>
+        <flux:badge color="violet">Violet</flux:badge>
+        <flux:badge color="purple">Purple</flux:badge>
+        <flux:badge color="fuchsia">Fuchsia</flux:badge>
+        <flux:badge color="pink">Pink</flux:badge>
+        <flux:badge color="rose">Rose</flux:badge>
     @fluxScripts
     </body>
 </html>
